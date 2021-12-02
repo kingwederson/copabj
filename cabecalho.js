f1d1233b054a7d216446a510603969ad9b19e38d
Wederson de Paula <56653848+kingwederson@users.noreply.github.com>
2021-12-02 18:56:24 -0300

    Add files via upload

--- cabecalho.js
+++ cabecalho.js
@@ -12,6 +12,7 @@ function cabi(){
                 <a href="http://copabj.wederson.net.br/ft/"><li>Futebol</li></a>
                 <a href="http://copabj.wederson.net.br/fs/"><li>Futsal</li></a>
                 <a href="http://copabj.wederson.net.br/fv/"><li>Futevôlei</li></a>
+                <a href="http://copabj.wederson.net.br/ma/"><li>Malha</li></a>
                 <a href="http://copabj.wederson.net.br/vl/"><li>Voleibol</li></a>
                 <a href="http://copabj.wederson.net.br/vp/"><li>Vôlei de Praia</li></a>
                 <a href="http://copabj.wederson.net.br/xz/"><li>Xadrez</li></a>
